4f5bdc08ceacfe4d0d88060fcfd4e3ff036ee823
nuntawat.rd <nuntawat.rd@gmail.com>
2025-11-20 12:10:57 +0000

    Auto Update: 2025-11-20 12:10:57

--- Nano/nano_cron.sh
+++ Nano/nano_cron.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-cd /home/nuntawat/mss2025-project-template/
+cd /home/nuntawt/mss2025-project-template/Nano/
 
-# 2. ตรวจสอบว่าเป็น Branch "Tongtong" หรือไม่ ถ้าไม่ใช่ให้ switch
 git checkout nano
 
-cd /home/nuntawt/mss2025-project-template/Nano/
 ./Student4_script.sh
-# 3. Add ไฟล์ทั้งหมด
+
 git add .
 
-# 4. Commit พร้อมระบุวันที่และเวลา เพื่อไม่ให้ข้อความซ้ำ
 git commit -m "Auto Update: $(date '+%Y-%m-%d %H:%M:%S')"
-
-# 5. Push ขึ้น Serve
 
 git push origin nano
